6c24c800e53d236b608a8b31b82aa058e69d08e7
Caleb Land <caleb@land.fm>
2016-11-09 15:18:31 -0500

    Allow the varnish storage to be configured

--- 5.0/docker-entrypoint.sh
+++ 5.0/docker-entrypoint.sh
@@ -2,6 +2,17 @@
 set -e
 
 if [ "${1}" == "varnish" ]; then
+  if [ -z "${STORAGE_SPEC}" ]; then
+    if [ ${STORAGE_KIND} = 'file' ]; then
+      touch "${STORAGE_FILE}"
+      chown varnish "${STORAGE_FILE}"
+
+      STORAGE_SPEC="${STORAGE_KIND},${STORAGE_FILE},${CACHE_SIZE}"
+    else
+      STORAGE_SPEC="${STORAGE_KIND},${CACHE_SIZE}"
+    fi
+  fi
+
   exec bash -c \
     "exec varnishd -j unix,user=varnish \
      -F \
